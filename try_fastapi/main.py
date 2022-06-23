e9664707da34aba6c2e735cf5f7daec5e10421fb
Thiago Veronezi <tveronezi@gmail.com>
2022-06-22 22:50:36 -0400

    /graphql

--- try_fastapi/main.py
+++ try_fastapi/main.py
@@ -1,7 +1,21 @@
 import uvicorn
+import strawberry
+
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def ping(self) -> str:
+        return "pong"
+
+schema = strawberry.Schema(Query)
+
+graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
+app.include_router(graphql_app, prefix="/graphql")
 
 
 @app.get("/v1/system/ping")
